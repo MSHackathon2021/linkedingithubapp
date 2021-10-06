fc024176c086223c494c458a146f66bb74f0635f
Arun Menon <arpn@microsoft.com>
2021-10-06 10:27:51 +0530

    Initial design changes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Home from './Home';
 import reportWebVitals from './reportWebVitals';
 
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Home />,
   document.getElementById('root')
 );
 
