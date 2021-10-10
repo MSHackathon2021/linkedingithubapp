42c272cc78702f5b71ee4f7c47f58d05a744065a
Arun Menon <arpn@microsoft.com>
2021-10-10 16:57:39 +0530

    Checked in with latest changes

--- src/Home.tsx
+++ src/Home.tsx
@@ -11,14 +11,14 @@ const axios = require('axios');
 
 export interface IHome {
   avatar: string | null;
-  isAuthenticated:boolean
+  isAuthenticated: boolean
   linkedinUserProfile: object | null;
-  gitHubUserPic:string ;
-  gitHubUserName:string ;
-  linkedinUserPic:string;
-  linkedinUserName:string;
-  repos:string[];
-  isAwaiting:boolean;
+  gitHubUserPic: string;
+  gitHubUserName: string;
+  linkedinUserPic: string;
+  linkedinUserName: string;
+  repos: string[];
+  isAwaiting: boolean;
 }
 
 
@@ -27,12 +27,12 @@ const Home: React.FunctionComponent = () => {
     avatar: null,
     isAuthenticated: false,
     linkedinUserProfile: null,
-    gitHubUserPic:"",
-    gitHubUserName:"",
-    linkedinUserPic:"",
-    linkedinUserName:"",
-    repos:[""],
-    isAwaiting:true
+    gitHubUserPic: "",
+    gitHubUserName: "",
+    linkedinUserPic: "",
+    linkedinUserName: "",
+    repos: [""],
+    isAwaiting: true
   });
 
   const [features, setFeatures] = useState({
@@ -45,46 +45,46 @@ const Home: React.FunctionComponent = () => {
 
   const fetchLinkedinUser = (temporaryUserId: string) => {
 
-    if(temporaryUserId!=null )
-       {
-        setUserData({...userData,isAwaiting:true});
-        axios.get('https://githublinkedinservice.azurewebsites.net/user', {
-          params: {
-            userId: temporaryUserId          }
-        }).then(function (response:any) {
-          console.log(response);
-          setUserData({
-            ...userData,
-            isAuthenticated: true,
-            gitHubUserPic:response.data.Git.avatar_url,
-            gitHubUserName:response.data.Git.name,
-            linkedinUserPic:response.data.Linkedin.Photo,
-            linkedinUserName:response.data.Linkedin.Displayname,
-            repos:response.data.Git.repos,
-            isAwaiting:false
-          });
-        })
+    if (temporaryUserId != null) {
+      setUserData({ ...userData, isAwaiting: true });
+      axios.get('https://githublinkedinservice.azurewebsites.net/user', {
+        params: {
+          userId: temporaryUserId
+        }
+      }).then(function (response: any) {
+        console.log(response);
+        setUserData({
+          ...userData,
+          isAuthenticated: true,
+          gitHubUserPic: response.data.Git.avatar_url,
+          gitHubUserName: response.data.Git.name,
+          linkedinUserPic: response.data.Linkedin.Photo,
+          linkedinUserName: response.data.Linkedin.Displayname,
+          repos: response.data.Git.repos,
+          isAwaiting: false
+        });
+      })
 
-       }
+    }
   };
 
-  const saveToSessionStorage=(key:string,temporaryUserId:string)=>{
+  const saveToSessionStorage = (key: string, temporaryUserId: string) => {
     try {
       localStorage.removeItem(key);
       window.localStorage.setItem(key, temporaryUserId);
-        }
-    catch(err){
-console.log(err)
-        }
+    }
+    catch (err) {
+      console.log(err)
+    }
   };
 
-  const FetchFromSessionStorage=(key:string)=>{
-    try { 
-     return window.localStorage.getItem(key);
-        }
-    catch(err){
-     return "";
-        }
+  const FetchFromSessionStorage = (key: string) => {
+    try {
+      return window.localStorage.getItem(key);
+    }
+    catch (err) {
+      return "";
+    }
   };
 
   const getParameterByName = (
@@ -92,19 +92,17 @@ console.log(err)
     url: string = window.location.href
   ) => {
 
-    var val=FetchFromSessionStorage(name);
+    var val = FetchFromSessionStorage(name);
     console.log(`tempid :${val}`)
-    if(val==null || val=="" )
-    {
-    name = name.replace(/[\[\]]/g, '\\$&');
-    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
-    results = regex.exec(url);
-    if (!results) return null;
-    if (!results[2]) return 'null';
-    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+    if (val == null || val == "") {
+      name = name.replace(/[\[\]]/g, '\\$&');
+      var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+        results = regex.exec(url);
+      if (!results) return null;
+      if (!results[2]) return 'null';
+      return decodeURIComponent(results[2].replace(/\+/g, ' '));
     }
-    else
-    {
+    else {
       return val;
     }
   };
@@ -112,12 +110,12 @@ console.log(err)
   //Initial render
   useEffect(() => {
     var temporaryUserId = getParameterByName('userdId');
-    if (temporaryUserId != null ) {
-    saveToSessionStorage("userdId",temporaryUserId);
-    fetchLinkedinUser(temporaryUserId);
+    if (temporaryUserId != null) {
+      saveToSessionStorage("userdId", temporaryUserId);
+      fetchLinkedinUser(temporaryUserId);
     }
-    else{
-      setUserData({...userData,isAwaiting:false});
+    else {
+      setUserData({ ...userData, isAwaiting: false });
     }
   }, []);
 
@@ -130,15 +128,15 @@ console.log(err)
         <h1>
           GitLinked
         </h1>
-            <Button
-              isAuthenticated={userData.isAuthenticated}
-               gitHubUserPic={userData.gitHubUserPic} 
-               gitHubUserName={userData.gitHubUserName} 
-               linkedinUserPic={userData.linkedinUserPic} 
-               linkedinUserName={userData.linkedinUserName}
-               Heading="Lets get started"
-               isAwaiting={userData.isAwaiting}
-            ></Button>
+        <Button
+          isAuthenticated={userData.isAuthenticated}
+          gitHubUserPic={userData.gitHubUserPic}
+          gitHubUserName={userData.gitHubUserName}
+          linkedinUserPic={userData.linkedinUserPic}
+          linkedinUserName={userData.linkedinUserName}
+          Heading="Lets get started"
+          isAwaiting={userData.isAwaiting}
+        ></Button>
       </section>
       <section className='medical-community'>
         <div className='.section-head'>
@@ -167,73 +165,14 @@ console.log(err)
             }
           </div>
           <div className='see-more'>
-            <a href='/'>Gurada tutti i medici della Community →</a>
+            <a href='https://garagehackbox.azurewebsites.net/hackathons/2356/projects/104280'>GitLinked →</a>
           </div>
         </div>
       </section>
       <section className='adv'>
         <div className='banner'>
           {' '}
-          <a href='/'>Metti la tua pubblicità su mediexpert.it</a>
-        </div>
-      </section>
-      <section className='section-wrap'>
-        <div className='medical-news'>
-          <div className='medium-title'>
-            Medicina News
-            <span />
-          </div>
-          <div className='posts'>
-            <div className='post'>
-              <div className='circle-icon red'>H</div>
-              <h3 className='post__title'>
-                <a href='#'>
-                  Humira, approvato in Europa per i bambini e gli adolescenti da
-                  4 anni di età con forma grave di psoriasi a placche cronica
-                </a>
-              </h3>
-            </div>
-            <div className='post'>
-              <div className='circle-icon'>A</div>
-              <h3 className='post__title'>
-                <a href='#'>
-                  Approvato in Europa Xadago nel trattamento della malattia di
-                  Parkinson moderata-grave con fluttuazioni motorie
-                </a>
-              </h3>
-            </div>
-            <div className='post'>
-              <div className='circle-icon violet'>P</div>
-              <h3 className='post__title'>
-                <a href='#'>
-                  Psoriasi a placche moderata-grave: BI 655066, un inibitore
-                  Il-23, superiore a Ustekinumab, un inibitore IL-12/23
-                </a>
-              </h3>
-            </div>
-            <div className='post'>
-              <div className='circle-icon blue'>A</div>
-              <h3 className='post__title'>
-                <a href='#'>
-                  Aducanumab riduce le placche amiloidi e rallenta la
-                  compromissione clinica nei pazienti con malattia di Alzheimer
-                  prodromica o lieve
-                </a>
-              </h3>
-            </div>
-            <div className='post'>
-              <div className='circle-icon'>E</div>
-              <h3 className='post__title'>
-                <a href='#'>
-                  Enalapril associato a Acido Folico riduce il rischio di ictus
-                  nei pazienti con ipertensione
-                </a>
-              </h3>
-            </div>
-          </div>
-          <div className='see-more'>
-            <a href='/'>Gurada tutte le news Xagena →</a>
-          </div>
+          <a href='https://garagehackbox.azurewebsites.net/hackathons/oneweek/2021/main'>Micosoft Global Hackathon</a>
         </div>
       </section>
     </div>
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,61 +2,78 @@ import '../css/Button.scss';
 import ContactCard from '../components/ContactCard';
 import { Spinner, SpinnerSize } from '@fluentui/react/lib/Spinner';
 import { IStackProps, Stack } from '@fluentui/react/lib/Stack';
+import { TooltipHost, ITooltipHostStyles } from '@fluentui/react/lib/Tooltip';
+import { useId } from '@fluentui/react-hooks';
 
 
 export interface IButtonProps {
   Heading?: string | null;
   selected?: boolean;
-  gitHubUserPic?:string ;
-  gitHubUserName?:string ;
-  linkedinUserPic?:string ;
-  linkedinUserName?:string;
-  isAuthenticated:boolean;
-  isAwaiting:boolean
+  gitHubUserPic?: string;
+  gitHubUserName?: string;
+  linkedinUserPic?: string;
+  linkedinUserName?: string;
+  isAuthenticated: boolean;
+  isAwaiting: boolean
 }
 
 const rowProps: IStackProps = { horizontal: true, horizontalAlign: 'center' };
 
-  const tokens = {
-    sectionStack: {
-      childrenGap: 10,
-    },
-    spinnerStack: {
-      childrenGap: 20,
-    },
-  };
+const tokens = {
+  sectionStack: {
+    childrenGap: 10,
+  },
+  spinnerStack: {
+    childrenGap: 20,
+  },
+};
+
+
+
+const calloutProps = { gapSpace: 0 };
+const hostStyles: Partial<ITooltipHostStyles> = { root: { display: 'inline-block' } };
+
 
 const Button = (props: IButtonProps) => {
+  const tooltipId = useId('tooltip');
   if (!props.isAuthenticated && !props.isAwaiting) {
     return (
+
       <div className='see-community'>
         <span>Bring GitHub and Linkedin closer.</span>
-      <div className='wrap'>
-        <a href="https://github.com/login/oauth/authorize?client_id=4390b5874ad74b454dc0" className='button'>
-        {props.Heading}
-        </a>
-      </div>
+        <TooltipHost
+          content="Click here to sync your github and linkedin profile"
+          id={tooltipId}
+          calloutProps={calloutProps}
+          styles={hostStyles}
+        >
+          <div className='wrap'>
+            <a href="https://github.com/login/oauth/authorize?client_id=4390b5874ad74b454dc0" className='button'>
+              {props.Heading}
+            </a>
+          </div>
+        </TooltipHost>
+
       </div>
     );
-  } 
-  else if(!props.isAuthenticated && props.isAwaiting)
-  {
-    return (<div className='loader'>
-    <Stack {...rowProps} tokens={tokens.spinnerStack}> 
-    <Spinner label="Please wait .. while we set things for you!" ariaLive="assertive" labelPosition="right" size={SpinnerSize.large} />
-  </Stack>
-  </div>
-  )
   }
-   else if(props.isAuthenticated) {
+  else if (!props.isAuthenticated && props.isAwaiting) {
+    return (<div className='loader'>
+      <Stack {...rowProps} tokens={tokens.spinnerStack}>
+        <Spinner label="Please wait .. while we set things for you!" ariaLive="assertive" labelPosition="right" size={SpinnerSize.large} />
+      </Stack>
+    </div>
+    )
+  }
+  else if (props.isAuthenticated) {
     return (
       <div>
         <h1><ContactCard gitHubUserPic={props.gitHubUserPic} gitHubUserName={props.gitHubUserName} linkedinUserPic={props.linkedinUserPic} linkedinUserName={props.linkedinUserName}></ContactCard></h1>
       </div>
     );
   }
-  else{
-    return(<></>)
+  else {
+    return (<></>)
   }
 };
 
